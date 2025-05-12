5f286e7147d695949f07d443c104d83f89024902
EricB-59 <ebaenac59@gmail.com>
2025-05-12 18:29:54 +0200

    update chat

--- src/components/MessageTemplate.tsx
+++ src/components/MessageTemplate.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 type Message = {
   author: string;
@@ -11,17 +11,32 @@ interface MessageTemplateProps {
 }
 
 function MessageTemplate({ message }: MessageTemplateProps) {
+  const [isCurrentUser, setIsCurrentUser] = useState(false);
+
+  useEffect(() => {
+    // Obtener datos del usuario del localStorage
+    const local = localStorage.getItem("user");
+    if (local) {
+      const userData = JSON.parse(local);
+      // Comprobar si el autor del mensaje es el mismo usuario actual
+      setIsCurrentUser(
+        userData.author === message.author || userData.name === message.author
+      );
+    }
+  }, [message.author]);
+
+  // Definir las clases dependiendo de si es el usuario actual o no
+  const bgColorClass = isCurrentUser ? "bg-blue-500" : "bg-secondary";
+
   return (
-    <div className="bg-special rounded-lg p-4 my-2 shadow-md">
+    <div className={`${bgColorClass} rounded-lg p-4 my-2 shadow-md`}>
       <div className="flex justify-between">
         <h2 className="font-bold">{message.author}</h2>
-        <span className="text-gray-400">
+        <span className="text-white">
           {new Date(message.timestamp).toLocaleTimeString()}
         </span>
       </div>
-      <h2 className="mt-2 break-words overflow-wrap-anywhere text-wrap">
-        {message.content}
-      </h2>
+      <h2 className="mt-2 break-words text-wrap">{message.content}</h2>
     </div>
   );
 }
